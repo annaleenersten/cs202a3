a32c0588924c8b3a608a34de3307f42fa13ac0df
annaleenersten <121985630+annaleenersten@users.noreply.github.com>
2024-01-25 19:56:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function changeText(){
+    document.getElementById('main-title').innerHTML = "Click to reveal answers!";
+    document.querySelector("header").style.background = "#b57de3";
+
+}
+
+function rotate(){
+    document.getElementById("gear").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#de40bc";
+    document.getElementById("text1").innerHTML = "A keyboard";
+}
+
+function rotate2(){
+    document.getElementById("gear2").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#fc8d2b";
+    document.getElementById("text2").innerHTML = "Tomorrow";
+}
+
+function rotate3(){
+    document.getElementById("gear3").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#f7f743";
+    document.getElementById("text3").innerHTML = "Light";
+}
+
+function rotate4(){
+    document.getElementById("gear4").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#3df550";
+    document.getElementById("text4").innerHTML = "A road";
+}
+
+function rotate5(){
+    document.getElementById("gear5").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#2eaadb";
+    document.getElementById("text5").innerHTML = "A stamp";
+}
+
+function rotate6(){
+    document.getElementById("gear6").style.transform = 'rotate(180deg)';
+    document.body.style.background = "#717ef5";
+    document.getElementById("text6").innerHTML = "A yardstick";
+}
